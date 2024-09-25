b2d1453f81e859d04e0c64a1998841a396944f51
skyedanler <skyedanler@gmail.com>
2024-09-25 11:01:23 -0400

    Organized files, initialized JS, CSS button events

---
+++ buttons/buttons.js
@@ -0,0 +1,8 @@
+
+let multiplierSingle = document.getElementById('single');
+let multiplierDouble = document.getElementById('double');
+let multiplierTriple = document.getElementById('triple');
+let multiplierQuadruple = document.getElementById('quadruple');
+let multiplierQuintuple = document.getElementById('quintuple');
+
+let spinButton = document.getElementById('spin');
---
+++ reels/reels.js
@@ -0,0 +1,8 @@
+//Symbols
+const javascript = './images/JS_symbol.png';
+const python = './images/python_symbol.png';
+const buggy = './images/bug_symbol.png';
+const curly_braces = './images/curly_brackets_symbol.png';
+const coffee = './images/coffee_cup.png';
+const parentheses = './images/parentheses.png';
+const semicolon = './images/semicolon.png';
