2b13d8ec34b48a2f75dca03c90c3583d0904df5a
skyedanler <skyedanler@gmail.com>
2024-09-27 13:57:10 -0400

    Added symbols to reels, started on the spin func

--- project.js
+++ project.js
@@ -23,8 +23,16 @@ multipliers.forEach(button => {
 });
 
 spinButton.addEventListener('click', function() {
-    useCredits(parseInt(document.querySelector('.multiplier.active').innerHTML, 10));
-    //you want to have it useCredits based on last multiplier clicked
+    useCredits(parseInt(document.querySelector('.multiplier.active').innerHTML, 10));//you want to have it useCredits based on last multiplier clicked
+    //spins reel with random amount of spins for each reel to ensure combinations aren't always the same
+    //TODO: buttons.disabled = true
+    spinReel(reel1, Math.random() * 20);
+    spinReel(reel2, Math.random() * 20);
+    spinReel(reel3, Math.random() * 20);
+    spinReel(reel4, Math.random() * 20);
+    spinReel(reel5, Math.random() * 20);
+    //TODO: buttons.disabled = false;
+    //TODO: updateCredits after spin happens
 });
 
 
@@ -37,6 +45,7 @@ function updateCredits(newCredits) {
 
 function useCredits(multiplier) { //subtracts credits depending on multiplier pressed
     let credits = parseInt(document.getElementById('credits').innerText, 10);
+    document.getElementById('cant-spin').innerHTML = '';
 
     if (multiplier <= credits) {
         credits -= multiplier;
@@ -44,8 +53,7 @@ function useCredits(multiplier) { //subtracts credits depending on multiplier pr
         enoughCredits(credits);
     } 
     else {
-        alert('Not enough credits to play this game.');
-        return;
+        document.getElementById('cant-spin').innerHTML = 'Nice try. You need more credits for that. Choose a different multiplier.';
     }
 };
 
@@ -62,3 +70,90 @@ function enoughCredits(credit_count) { //check if there are enough credits for e
         }
     })
 }
+
+
+
+
+//reels
+//Symbols
+const JS = './images/JS_symbol.png';
+const python = './images/python_symbol.png';
+const buggy = './images/bug_symbol.png';
+const curly_braces = './images/curly_brackets_symbol.png';
+const coffee = './images/coffee_cup.png';
+const parentheses = './images/parentheses.png';
+const semicolon = './images/semicolon.png';
+
+//each reel has the following amount of symbols
+const symbolAmount = {
+    JS: 1,
+    python: 2,
+    buggy: 4,
+    curly_braces: 3,
+    coffee: 1,
+    parentheses: 3,
+    semicolon: 3
+}
+
+function createReel(symbolAmount) {
+    let symbols = [];
+    for (let symbol in symbolAmount) {
+        for (let i = 0; i < symbolAmount[symbol]; i++) {
+            symbols.push(eval(symbol));
+        }
+    }
+    symbols = symbols.sort(() => Math.random() - 0.5); //for randomness so that they aren't shuffling through in the same way on the slot machine
+    return symbols;
+}
+
+// creating all reels with the same amount of symbols based on paytable combination probability
+reel1 = createReel(symbolAmount);
+reel2 = createReel(symbolAmount);
+reel3 = createReel(symbolAmount);
+reel4 = createReel(symbolAmount);
+reel5 = createReel(symbolAmount);
+
+//function to only display three symbols per reel at a time
+function displayReel(reel, symbols) { // TODO: I want five reel divs separated into 3 sections because images must align with those
+    reel.innerHTML = '';
+   
+    let selectedSymbols = symbols.slice(0, 3);
+    selectedSymbols.forEach(symbol => {
+        let img = document.createElement('img');
+        img.src = symbol;
+        img.classList.add('reel-image');//create class so I can use CSS
+        reel.append(img);
+    });   
+}
+
+
+
+//function to spin the reels
+function spinReel(reel, spins) {
+
+    let pulledValue = 0;
+
+    while (spins > 0) {
+        pulledValue = reel.shift();
+        reel.append[pulledValue];
+        spin -= 1;
+    }
+    return reel;
+}
+
+
+//displayed reels
+displayReel(document.getElementById('reel1'), reel1);
+displayReel(document.getElementById('reel2'), reel2);
+displayReel(document.getElementById('reel3'), reel3);
+displayReel(document.getElementById('reel4'), reel4);
+displayReel(document.getElementById('reel5'), reel5);
+
+
+let symbolCombo = [];//create array of the five symbol combination it lands on for use in wonCredits
+//function to updateCredits based on combination win/not win
+function wonCredits(symbolCombo) {
+    
+}
+
+//TODO: perhaps create some functions or quick for loops to do functions for each reel. Gets repetitive
